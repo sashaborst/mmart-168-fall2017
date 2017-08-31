bf00240ad01ca31aa156e76f0931c0a8e2fbd99a
Sarah <vanwars@gmail.com>
2017-08-31 12:50:21 -0700

    Update mmart168-installation.sh

--- utilities/mmart168-installation.sh
+++ utilities/mmart168-installation.sh
@@ -44,10 +44,10 @@ echo 'What is homebrew?: Homebrew is a package manager for Mac. Read more here: 
 echo 'https://brew.sh/'
 echo '--------------------------------------------------------------------------'
 
-sudo chown -R $(whoami) /usr/local/var/homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew update
 sudo chown -R $(whoami) /usr/local/var/homebrew
+sudo chown -R $(whoami) /usr/local/Homebrew/
+brew update
 
 echo ''
 echo ''
