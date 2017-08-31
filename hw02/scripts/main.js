8c0ea27d4c08205aae4dfd6aa909f74c271bd304
Sarah <vanwars@gmail.com>
2017-08-31 15:28:02 -0700

    hw modifications

--- hw02/scripts/main.js
+++ hw02/scripts/main.js
@@ -1,14 +1,20 @@
 const modifyText = () => {
   // YOUR CODE HERE
+  document.getElementById("freebie").innerHTML = "Example Freebie!"
   console.log("modifying text")
 }
 
-const changeAttribute = () => {
+const changeLinkAndImageAttributes = () => {
   // YOUR CODE HERE
-  console.log("changing attribute")
+  console.log("changing link attribute")
 }
 
-const appendElement = () => {
+const changeClassAttribute = () => {
+  // YOUR CODE HERE
+  console.log("changing class attribute")
+}
+
+const appendElements = () => {
   // YOUR CODE HERE
   console.log("appending element")
 }
