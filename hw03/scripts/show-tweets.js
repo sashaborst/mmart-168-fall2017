39f2ba3019d1ee52d77481671966834d9e91b222
Sarah <vanwars@gmail.com>
2017-09-07 13:30:29 -0700

    Updated Homework

--- hw03/scripts/show-tweets.js
+++ hw03/scripts/show-tweets.js
@@ -7,27 +7,27 @@ const tweet1 = () => {
 }
 
 const tweet2 = () => {
-    console.log(tweets.statuses[1])
-    document.querySelector("#tweet2 .message").innerHTML = tweets.statuses[1].text
+    //TODO: output the contents of the second tweet below the first one:
 }
 
 const tweet3 = () => {
-    console.log(tweets.statuses[2])
-    document.querySelector("#tweet1 .message").innerHTML = tweets.statuses[2].text
+    //TODO: output the contents of the third tweet below the second one:
 }
 
+const showAll = () => {
+    //https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
 
-const appendTweet = (id) => {
-    console.log(id)
-    console.log(tweets.statuses[id])
-    document.querySelector(".main").innerHTML += '<article id="tweet' + (id + 1) + '" class="tweet"> \
-        <i class="fa fa-user"></i> \
-        <a href="#"></a> \
-        <span class="username"></span> &bull; \
-        <span class="date"></span> \
-        <div class="message"></div> \
-    </article>'
-    document.querySelector("#tweet" + (id + 1) + " .message").innerHTML = tweets.statuses[id].text
-    document.querySelector("#tweet" + (id + 1) + " .username").innerHTML = tweets.statuses[id].user.screen_name
-    document.querySelector("#tweet" + (id + 1) + " .date").innerHTML = tweets.statuses[id].created_at
+    // tweet 1:
+    var newDiv = document.createElement("div");
+    var newContent = document.createTextNode(tweets.statuses[0].text);
+    newDiv.appendChild(newContent); //add the text node to the newly created div.
+    document.getElementById("all").appendChild(newDiv);
+
+    // tweet 2:
+    newDiv = document.createElement("div");
+    newContent = document.createTextNode(tweets.statuses[1].text);
+    newDiv.appendChild(newContent); //add the text node to the newly created div.
+    document.getElementById("all").appendChild(newDiv);
+
+    //.....
 }
