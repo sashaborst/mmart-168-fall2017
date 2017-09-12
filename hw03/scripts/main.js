3e8aebf699586ebdc858246047e139af99e57fbd
Sarah <vanwars@gmail.com>
2017-09-11 18:21:15 -0700

    edited readme file

--- hw03/scripts/main.js
+++ hw03/scripts/main.js
@@ -7,13 +7,21 @@ const beyonceTweet = {
 const links = ['https://twitter.com/mmart168', 'https://twitter.com/beyonce', 'http://www.billboard.com/files/media/beyonce-2016-press-Parkwood-Entertainment-Lemonade-billboard-1548-650-4.jpg']
 
 const modifyText = () => {
-  // YOUR CODE HERE
-  document.querySelector('#freebie').innerHTML = 'Example Freebie!'
-  console.log('modifying text')
+  // Part 1.a. Changes the text of message2 to be the message of the BeyonceTweet object
+  document.querySelector('#message2').innerHTML = beyonceTweet.text
+
+  // Part 1.b. Change the text of link2 --> text of tweet
+  document.querySelector('#link2').innerHTML = beyonceTweet.name
+  //           Change the text of user2 --> username of tweet
+  document.querySelector('#user2').innerHTML = beyonceTweet.handle
+  //           <span id="user2">@bynoce</span>
+
+
 }
 
 const changeLinkAndImageAttributes = () => {
   // YOUR CODE HERE
+  document.querySelector('#message2').innerHTML = 'blah'
   console.log('changing link attribute')
 }
 
