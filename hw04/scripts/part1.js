73959736cfbf2798566f5d099d259998b2f6460c
Student <student@227MAC07.local>
2017-09-21 21:44:40 -0700

    Edit languages in the ***setLanguage*** function

--- hw04/scripts/part1.js
+++ hw04/scripts/part1.js
@@ -3,15 +3,18 @@
 let language
 let languageCode
 
+//Edit the ***setLanguage*** function so that it handles the new language codes and
+// displays the correct message to the span element (inside the h1 element).
+
 const setLanguage = (code) => {
     //Note: language codes here: https://www.w3schools.com/tags/ref_language_codes.asp
     languageCode = code
-    if (code === 'ru') {
-        language = 'Russian'
-    } else if (code === 'es') {
-        language = 'Spanish'
+    if (code === 'zh') {
+        language = 'Chinese'
+    } else if (code === 'cs') {
+        language = 'Czech'
     } else {
-        language = 'English'
+        language = 'Croatian'
     }
     document.getElementById('language').innerHTML = language
 }
