bf1daf93813057e0ab839745fb98c07fde1007d8
Sarah <vanwars@gmail.com>
2017-10-14 14:22:57 -0700

    adding hw07

---
+++ hw07/js/index.js
@@ -0,0 +1,29 @@
+//------------------------------------------------------------------
+// PART I
+//------------------------------------------------------------------
+// Create a function that appends a "li" element to an unordered list ("ul tag") in the DOM. This function should take a string as a parameter and append the string to the un-ordered list:
+
+//Example: If there is a ul tag in the DOM...
+// <ul id="output">
+//       <li>string #1</li> <-- add string here within an "li" tag
+//       <li>string #2</li> <-- add string here within an "li" tag
+//  </ul>
+
+
+//------------------------------------------------------------------
+// PART II
+//------------------------------------------------------------------
+// Using the strategy of your choice, write a loop over the numbers 1 - 100 that satisfies the following conditions:
+// 1. If the number is divisible by 3, append "Fizz" to the unordered list
+// 2. If the number is divisible by 5, append "Buzz" to the unordered list
+// 3. If the number is divisible by 3 AND 5, append "FizzBuzz" to the unordered list.
+
+//Hint:
+// 1. Take a look at the math functions: https://www.w3schools.com/js/js_arithmetic.asp
+// 2. Take a look at some loop references:
+//   * https://www.lynda.com/JavaScript-tutorials/Loops/574716/612083-4.html
+//   * https://www.w3schools.com/js/js_loop_for.asp
+
+for (let i=1; i <= 100; i++) {
+  console.log(i);
+}
