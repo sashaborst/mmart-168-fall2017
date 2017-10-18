4748ced617969d5186cbf8c757f8b6cc41e355da
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2017-10-18 13:46:46 -0700

    Complete homework 7

--- hw07/js/index.js
+++ hw07/js/index.js
@@ -7,15 +7,37 @@
 // appends an "li" element, with the string inside, to the un-ordered
 // list ("ul" tag):
 
-
-
+// document.getElementById('output').append['<li>Hello!</li>']
+const addLiElement = (message) => {
+// Create a <li> node
+const element = document.createElement('li')
+// Create a text node
+const textnode = document.createTextNode(message)
+// Append the text to <li>
+element.appendChild(textnode)
+// Append <li> to <ul>
+const ul = document.getElementById('output')
+ul.appendChild(element)
+}
 //------------------------------------------------------------------
 // PART II
 //------------------------------------------------------------------
 // Using the strategy of your choice, write a loop over the numbers
 // 1 - 100 that satisfies the following conditions:
-//
+for (let i = 1; i <= 100; i++ ) {
 // 1. If the number is divisible by 3, append "Fizz" to the unordered list
+if ( i % 3 === 0 && i % 5 === 0) {
+  addLiElement('FizzBuzz')
+}
 // 2. If the number is divisible by 5, append "Buzz" to the unordered list
+else if ( i % 5 === 0) {
+  addLiElement('Buzz')
+}
+else if (i % 3 === 0)   {
+addLiElement('Fizz')
+}
+else { addLiElement(i)}
 // 3. If the number is divisible by 3 AND 5, append "FizzBuzz" to the unordered list.
 // 4. Bonus points: Include both the number *and* the word you've appended to the list
+
+}
