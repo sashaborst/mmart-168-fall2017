4fe6375db583444d6465872e90e5292e6e938510
Sarah <vanwars@gmail.com>
2017-11-02 17:50:46 -0700

    Homework 10 stub

---
+++ hw10/README.md
@@ -0,0 +1,39 @@
+## HOMEWORK 10: MORE PRACTICE WITH LOOPS
+> * Due date: Midnight on Wednesday, 10/25 (1 week)
+> * Part I: Setting up your new homework branch
+> * Part II: Coding Homework
+> * Part III: Submit
+
+## Part I: GitHub Setup
+1. Using the Terminal, navigate to your mmart-168-fall2017 git repository: `$ cd {your git repository path} #different for everyone`
+2. Check out the master branch: `$ git checkout master`
+3. Pull the most recent changes from the upstream repository: `$ git pull --rebase upstream master`
+4. Create a new branch to do Homework 10: `$ git branch homework10`
+5. Checkout your new homework10 branch: `$ git checkout homework10`
+6. Verify that you are now on the new branch: `$ git branch` (check that your new homework10 branch has an asterisk next to it).
+
+## Part II: Browsing the BART API Documentation
+Open the BART API Documentation, and take a look at the kinds of information the API
+offers: https://api.bart.gov/docs/overview/examples.aspx. Consider the following:
+* What kind of information does this API make available?
+* What apps and websites do you think access this information?
+* Exercise (not to turn in): Open the following URL in your web browser:
+  https://api.bart.gov/api/sched.aspx?cmd=depart&orig=24th&dest=rock&key=MW9S-E7SL-26DU-VV8V&json=y
+    * What happens when you change the "orig" and "dest" parameters (see this list: https://api.bart.gov/docs/overview/abbrev.aspx)?
+    * What happens when you remove the "json=y" section of the URL?
+
+## PART III: Coding
+
+### A. Building the Station List Menu
+Using any loop you want, modify the makeStationList function so that it populates the select menu with *ALL* of the stations that are returned from the BART data feed: https://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y
+
+### B. Writing to the DOM, using a Loop
+Using a for loop, write out each person’s avatar, name, and score, based on the **people** array
+
+
+## Part III: Submit
+1. Just as in Part II, open your Terminal and type: `$ git status`. It should tell you which files you've changed.
+2. To "commit" all of your changes, type: ` $ git commit -am 'Completed homework 8'`
+3. To "push" your change to up to your github account, type: `$ git push --set-upstream origin homework08`
+4. Finally, verify that your changes are up on GitHub by logging into your GitHub account and taking a look at your newly created homework08 branch.
+5. **Submit your homework by pasting a link to your branch into the Moodle.**
---
+++ hw10/js/main.js
@@ -0,0 +1,63 @@
+const apiKey = 'MW9S-E7SL-26DU-VV8V'
+
+const makeStationList = () => {
+    const url = 'https://api.bart.gov/api/stn.aspx?key=' + apiKey +
+                    '&cmd=stns&json=y'
+    fetch(url)
+        .then((response) => {
+            return response.json()
+        })
+        .then((json) => {
+            json = json.root
+            console.log(json)
+
+            //PART III.A.: Use a loop to populate the select menu with *ALL*
+            // of the stations that are returned from the BART data feed:
+            const option1 = document.createElement("option")
+            option1.value = 'DBRK'
+            option1.innerHTML = 'Downtown Berkeley'
+            document.getElementById('station_list').appendChild(option1)
+        })
+}
+
+const getArrivalTimes = () => {
+    const stationList = document.getElementById('station_list')
+    const bartStationCode = 'DBRK'
+    console.log('Selected Station Code:', bartStationCode)
+    let url = 'https://api.bart.gov/api/etd.aspx?key=' + apiKey + '&cmd=etd' +
+                '&orig=' + bartStationCode + '&json=y'
+    fetch(url)
+        .then((response) => {
+            return response.json()
+        })
+        .then((json) => {
+            json = json.root
+            console.log(json)
+            const results = document.getElementById('results')
+            results.innerHTML = ''
+            json.station = json.station[0]
+            if (!Array.isArray(json.station.etd)) {
+                json.station.etd = [ json.station.etd ]
+            }
+            json.station.etd.forEach(trainLine => {
+                if (!Array.isArray(trainLine.estimate)) {
+                    trainLine.estimate = [ trainLine.estimate ]
+                }
+                console.log('------------------------------------------------------------------------')
+                console.log('FROM:', stationList.options[stationList.selectedIndex].text.toUpperCase())
+                console.log('TO:', trainLine.destination.toUpperCase())
+                console.log('------------------------------------------------------------------------')
+                trainLine.estimate.forEach(estimate => {
+                    console.log(
+                        ' * Direction:', estimate.direction,
+                        ', Leaving: ', estimate.minutes,
+                        ', Color: ', estimate.hexcolor,
+                        ', Platform:', estimate.platform,
+                        ', Delay?:', estimate.delay
+                    )
+                })
+            })
+        })
+}
+
+makeStationList()
