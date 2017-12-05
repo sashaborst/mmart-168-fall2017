c23f9820df334c6ef19968cd8568361dcc132857
Sarah <vanwars@gmail.com>
2017-12-04 17:49:46 -0800

    roberta clarfai part 2

--- samples/clarifai/main.js
+++ samples/clarifai/main.js
@@ -1,20 +1,25 @@
 const app = new Clarifai.App({
     apiKey: 'c89a5c260af54d7686af7e3afcbc70ec'
 });
-const imageURL = 'https://vignette.wikia.nocookie.net/monster/images/6/6e/DragonRed.jpg/revision/latest/scale-to-width-down/1000'
-app.models.predict(Clarifai.GENERAL_MODEL, imageURL).then(
-  function(response) {
-    console.log(response.rawData.outputs[0].data.concepts);
-    const concepts = response.rawData.outputs[0].data.concepts
-    concepts.forEach((concept) => {
-        const p = document.createElement('p')
-        const textNode = document.createTextNode(concept.name)
-        p.appendChild(textNode)
-        document.getElementById('results').appendChild(p)
-        console.log(concept.name)
-    })
-  },
-  function(err) {
-    console.error(err);
-  }
-);
+
+const identify = (elem) => {
+    const imageURL = elem.src
+    app.models.predict(Clarifai.GENERAL_MODEL, imageURL).then(
+        function(response) {
+        console.log(response.rawData.outputs[0].data.concepts);
+        const concepts = response.rawData.outputs[0].data.concepts
+        document.getElementById('results').innerHTML = ""
+        concepts.forEach((concept) => {
+            const p = document.createElement('p')
+            const textNode = document.createTextNode(concept.name)
+            p.appendChild(textNode)
+            document.getElementById('results').appendChild(p)
+            console.log(concept.name)
+        })
+      },
+      function(err) {
+        console.error(err);
+      }
+    );
+
+}
