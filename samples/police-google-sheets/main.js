fa5e689a0601cf1f7c6756d381dd8f8dd3e646db
Sarah <vanwars@gmail.com>
2017-12-07 20:06:50 -0800

    updates

--- samples/police-google-sheets/main.js
+++ samples/police-google-sheets/main.js
@@ -1,12 +1,23 @@
 const sheets = new Sheets({
     key: '1kTtU8Me3ADNl2fd44cV9KK4GvFFLMqeyOZ-qy5bKvUw',
-    query: 'select E, count(A) group by E'
+    query: 'select E, count(A) group by E order by count(A) desc'
 });
 
+/*sheets.getData(data => {
+    data.records.forEach(record => {
+        //1. create the list item:
+        const li = document.createElement('li')
+        li.innerHTML = record.st_ + ': ' + record.count_original_data_format
+        document.getElementById('unordered-list').appendChild(li)
+    })
+});*/
+
 sheets.getData(data => {
-    //loop through records and output to the screen:
-    console.log(data)
-    /*data.records.forEach(record => {
-        console.log(record)
-    })*/
+    data.records.forEach(record => {
+        //1. create the list item:
+        const div = document.createElement('div')
+        div.innerHTML = '<div class="bar" style="width: ' + 3 * record.count_original_data_format + 'px"></div>'
+        div.innerHTML += ' ' + record.st_
+        document.getElementById('container').appendChild(div)
+    })
 });
